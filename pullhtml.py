729661979cfa38da0e6cf1cc618c7b993566bd3c
Evan <evan.v.gipson@wmich.edu>
2010-03-25 13:10:54 -0400

    revised parsing

--- pullhtml.py
+++ pullhtml.py
@@ -8,14 +8,17 @@ import urllib
 #   -have link for file download
 #   -http://cs5550.webs.com/aint_no_rest.tracker
 f = urllib.urlopen("http://cs5550.webs.com/aint_no_rest.tracker")
-s = f.read()
-f.close()
-
-print s
 #read the file
-#   -in /p2p/tracker
-
+s = f.read().split('\n')
+f.close()
+#initializing lists
+ip = []
+part = []
 #get the list of IP's
+newlist = [i.split(',') for i in s]
+#get the last entry out of there
+newlist.pop()
+
 #create a socket
 #connect to the IP's
 #populate the file list
